fa2b9b3f45b07a753ab747b48b47b1c127f7e560
Jon Moss <me@jonathanmoss.me>
2015-05-03 02:00:26 +0000

    [Umbrella] some reorganization

--- umbrella/spec/controllers/api/v1/users_controller_spec.rb
+++ umbrella/spec/controllers/api/v1/users_controller_spec.rb
@@ -24,28 +24,4 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
     end
   end
-  
-  describe 'PUT #update' do
-    before do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-    end
-    
-    context "with valid attributes" do
-      it "updates a user" do
-        put :update, user_id: @user, user: FactoryGirl.attributes_for(:user, email: "test1@example.com")
-        @user.reload
-        @user.email.should eq("test1@example.com")
-      end
-      it "updates a user, making sure response is #200" do
-        post :create, user: FactoryGirl.attributes_for(:user), format: :json
-        @response.status.should eq(201)
-      end
-    end
-    context "with invalid attributes" do
-      it "returns 422" do
-        post :create, user: FactoryGirl.attributes_for(:user, email: nil, password: nil), format: :json
-        @response.status.should eq(422)
-      end
-    end
-  end
 end
